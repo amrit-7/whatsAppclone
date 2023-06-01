d9950496b68a05354b10c1e6362570555f634ad7
amrit <gillamrit29@gmail.com>
2023-06-01 17:42:49 +0530

    qr in ejs

--- index.js
+++ index.js
@@ -1,18 +1,62 @@
 const express = require("express");
-const qr = require("qrcode");
+const app = express();
+const ejs = require("ejs");
+const qrc = require("qrcode");
+const qrcode = require("qrcode-terminal");
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+const { Client } = require("whatsapp-web.js");
+const client = new Client();
 
-const data = {
-  name: "Amritpal",
-  age: "23",
-};
-const jsonQr = JSON.stringify(data);
-qr.toString(jsonQr, { type: "terminal" }, (err, qrString) => {
-  if (!err) {
-    console.log("🚀 ~ file: index.js:9 ~ qrString:", qrString);
-  }
+app.get("/", (req, res) => {
+  // console.log(qrcode);
+  client.on("qr", (qr) => {
+    qrc.toDataURL(qr, (err, url) => {
+      if (err) {
+        console.log("hello", err);
+      } else {
+        res.render("home.ejs", { code: url });
+      }
+    });
+  });
+  client.on("ready", () => {
+    console.log("Client is ready!");
+  });
+
+  client.on("message", (message) => {
+    console.log(message.body);
+  });
+  client.on("message", (message) => {
+    if (message.body === "!ping") {
+      message.reply("pong");
+    }
+  });
+
+  client.initialize();
+
+  // const data = {
+  //   name: "Amritpal",
+  //   age: "23",
+  // };
+  // const jsonQr = JSON.stringify(data);
+  // qr.toString(jsonQr, { type: "terminal" }, (err, qrString) => {
+  //   if (!err) {
+  //     console.log("🚀 ~ file: index.js:9 ~ qrString:", qrString);
+  //   }
+  // });
+  // const newQR = await qr.toDataURL(
+  //   "qr.png",
+  //   jsonQr,
+  //   { type: "terminal" },
+  //   (err) => {
+  //     if (err) {
+  //       console.log("🚀 ~ file: index.js:9 ~ ERROR:", err);
+  //     }
+  //   }
+  // );
+  // console.log("🚀 ~ file: index.js:23 ~ newQR ~ newQR:", newQR);
 });
-qr.toFile("qr.png", jsonQr, { type: "terminal" }, (err, qrString) => {
-  if (!err) {
-    console.log("🚀 ~ file: index.js:9 ~ qrString:", qrString);
-  }
+
+app.listen(3000, () => {
+  console.log("Server at 3000");
 });
